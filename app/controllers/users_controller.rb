ef4370b15b8bcf2f30e1474c9f7b544bc48931dc
Andrew Delmar <andrewjdelmar@gmail.com>
2020-07-23 01:25:30 +0000

    Added user auth but ran into bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,7 @@
 class UsersController < ApplicationController
-
-  def new
-  end
   
   def create
+    puts user_params
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -13,9 +11,12 @@ class UsersController < ApplicationController
     end
   end 
 
-    private
+  def new
+  end
 
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end 
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end 
 end
